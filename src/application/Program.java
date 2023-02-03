a7b358317891f4f7aceab7973400ad7ddaa7ec04
eliwelton silva <eliwelton.rc@gmail.com>
2023-02-03 15:49:26 -0300

    up

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,10 +10,21 @@ public class Program {
 		
 		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
 		
-		Stream<Integer> st1 = list.stream(); // Cria uma Stream apartir da lista
+		// Cria uma Stream apartir da lista
+		Stream<Integer> st1 = list.stream().map(x -> x *10);//aplica função a cada elemento da stream
 		
 		System.out.println(Arrays.toString(st1.toArray/*converte stream para vetor*/()));
 
+		
+		Stream <String> st2 = Stream.of/*permite colocar os elemntos em parênteses*/ ("Maria", "Alex", "Bob");
+		System.out.println(Arrays.toString(st2.toArray()));
+		
+		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
+		System.out.println(Arrays.toString(st3.limit(10).toArray()));
+		
+		Stream<Long> st4 = Stream.iterate(new Long[] {0L, 1L}, p -> new Long[] {p[1], p [0]+p[1]}).map(p -> p[0]);// java n possui tupla
+		System.out.println(Arrays.toString(st4.limit(10).toArray()));
+				
 	}
 
 }
